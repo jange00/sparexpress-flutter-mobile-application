561050e5a5aaca4834b85ebfbe134f5c46c7709c
Sushant Mahato <sushantmahato@Sushants-MacBook-Air.local>
2025-05-17 20:03:48 +0545

    Added bottomNavigation on dashboard screen

--- lib/view/dashboard_screen.dart
+++ lib/view/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sparexpress/view/home/home_page.dart';
 
 import 'package:sparexpress/view/login_screen.dart';
 
@@ -14,7 +15,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
   int _selectedIndex = 0;
   final Color themeColor = const Color(0xFFFFC107);
 
-  
 
   void _onItemTapped(int index) {
     setState(() => _selectedIndex = index);
@@ -40,8 +40,66 @@ class _DashboardScreenState extends State<DashboardScreen> {
         foregroundColor: Colors.black,
         elevation: 3,
       ),
+
       
+       
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: themeColor,
+        unselectedItemColor: Colors.grey[600],
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Your Cart is empty', style: TextStyle(fontSize: 18)),
     );
   }
 }
 
+class OrdersPage extends StatelessWidget {
+  const OrdersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Your Orders', style: TextStyle(fontSize: 18)),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Profile Settings', style: TextStyle(fontSize: 18)),
+    );
+  }
+}
